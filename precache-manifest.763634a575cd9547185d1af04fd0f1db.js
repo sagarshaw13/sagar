a51e044424101280a578daef980b066321481bb3
saggy2001 <sagarshaw2001@gmail.com>
2023-02-16 00:45:51 +0530

    Updates

---
+++ precache-manifest.763634a575cd9547185d1af04fd0f1db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd497e55cbf27dc3ae21d40609bdbcb",
+    "url": "/sagar/index.html"
+  },
+  {
+    "revision": "c4bcac360148fdb646d7",
+    "url": "/sagar/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "67968e09c8ffd89e5bd3",
+    "url": "/sagar/static/css/main.70e1369f.chunk.css"
+  },
+  {
+    "revision": "c4bcac360148fdb646d7",
+    "url": "/sagar/static/js/2.0dd9bf5e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/sagar/static/js/2.0dd9bf5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67968e09c8ffd89e5bd3",
+    "url": "/sagar/static/js/main.b0e55590.chunk.js"
+  },
+  {
+    "revision": "ddae21e206f5f1302dfe",
+    "url": "/sagar/static/js/runtime-main.9481efca.js"
+  }
+]);
